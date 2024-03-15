f950b65979e01762bc5b414c22f272ffbd0d9212
CoderJoeW <jwilliamson@questblue.com>
2024-03-15 12:17:03 -0400

    integration of trained model

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ import cv2
 import pyautogui
 import time
 import GameState
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+
+# Load the model
+model = load_model('wizard101_navigation_model.h5')
+
+def prepare_image(frame, target_size=(256, 256)):
+    img = cv2.resize(frame, target_size)  # Resize the image
+    img_array = img_to_array(img)  # Convert to array
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+    img_array /= 255.0  # Normalize the pixel values if your model expects values in [0, 1]
+    return img_array
+
+def predict_direction(frame):
+    prepared_img = prepare_image(frame)
+    predictions = model.predict(prepared_img)
+    class_indices = {'left': 0, 'right': 1, 'forward': 2, 'arrived': 3}
+    labels = list(class_indices.keys())
+    predicted_class = labels[np.argmax(predictions)]
+    return predicted_class
 
 def capture_screen():
     screenshot = pyautogui.screenshot()
@@ -11,16 +31,12 @@ def capture_screen():
     return frame
 
 def is_in_battle():
-    # Essentially if the spellbook is present on the screen
-    # We can assume that we are not in a battle
     game_state = GameState.GameState()
     screen = capture_screen()
     game_state.update_state(screen)
     return not getattr(game_state, 'spellbook', False)
 
 def is_our_turn():
-    # We are assuming if we can see the pass button
-    # It is our turn to pick a card or pass
     game_state = GameState.GameState()
     screen = capture_screen()
     game_state.update_state(screen)
@@ -35,6 +51,11 @@ def main():
 
         in_battle = is_in_battle()
         print(f"In Battle: {in_battle}")
+
+        # If not in battle, predict and print the direction
+        if not in_battle:
+            direction = predict_direction(screen)
+            print(f"Direction: {direction}")
 
         time.sleep(0.5)
 
